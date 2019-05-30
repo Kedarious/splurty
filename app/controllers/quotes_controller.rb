139fb448f8c7bc9675e314911aa0892f013560dc
kedarious <darious_petway@yahoo.com>
2019-05-30 03:25:17 +0000

    whatever the message

--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -4,5 +4,23 @@ class QuotesController < ApplicationController
 	end
 
 	 def new
+	 	@quote = Quote.new
   end
+
+  def create
+  	quote.create(quote_params)
+  	if @quote.invalid?
+  		flash[:error] = '<strong>Could not save</strong> the data you entered is invalid.'
+  	end
+  	redirect_to root_path
+  end
+
+  def about
+  end
+
+  private
+
+  def quote_params
+  	params.require(:quote).permit(:saying, :author)
+  end	
 end
